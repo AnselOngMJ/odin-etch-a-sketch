64aadc9247f43ffc84c8507b68ff1650af54bfdd
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-19 21:34:26 +0000

    Add function to use random RGB values for grid and to darken them

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const container = document.querySelector('#container');
 
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
 function createGrid(size) {
     const percentage = 100 / size;
-    console.log(percentage)
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.className = 'square';
         container.append(square);
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black';
+            let colours = [];
+            if (square.style.backgroundColor) {
+                const bg = square.style.backgroundColor;
+                colours = bg.slice(4).slice(0, bg.length - 5).split(', ');
+                colours = colours.map((colour) => Math.max(0, colour - 25.5));
+            } else {
+                for (let i = 0; i < 3; i++) {
+                    colours.push(randomColor());
+                }
+            }
+            square.style.backgroundColor = `
+                rgb(${colours[0]}, ${colours[1]}, ${colours[2]})
+            `;
         });
         square.style.flex = `1 0 ${percentage}%`;
         square.style.height = `${percentage}%`;
