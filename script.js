3082dbcfae76b61ba95143647b7766cf73cc7ea6
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2023-12-19 21:05:57 +0000

    Create function to customise grid size

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector('#container');
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    container.append(square);
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
+
+function createGrid(size) {
+    const percentage = 100 / size;
+    console.log(percentage)
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        container.append(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+        square.style.flex = `1 0 ${percentage}%`;
+        square.style.height = `${percentage}%`;
+    }
 }
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let size = prompt('Number of squares per side: ');
+    size = Math.min(size, 100);
+    container.innerHTML = '';
+    createGrid(size);
+})
+
+createGrid(16);
